aa5568f323491a1b5d133d67ebd92d67fbf5e596
Matěj Cepl <mcepl@redhat.com>
2013-04-22 02:50:45 +0200

    Fix Python scripts to be PEP-8 compatible.
    Fixes #24

--- vimoutliner/scripts/otlgrep.py
+++ vimoutliner/scripts/otlgrep.py
@@ -7,49 +7,49 @@
 ###########################################################################
 # Basic function
 #
-#	This program searches an outline file for a branch that contains
-#	a line matching the regex argument. The parent headings (branches)
-#	and the children (sub-branches and leaves) of the matching headings
-#	are returned.
+#    This program searches an outline file for a branch that contains
+#    a line matching the regex argument. The parent headings (branches)
+#    and the children (sub-branches and leaves) of the matching headings
+#    are returned.
 #
-#	Examples
+#    Examples
 #
-#	Using this outline:
+#    Using this outline:
 #
-#	Pets
-#	Indoor
-#		Cats
-#			Sophia
-#			Hillary
-#		Rats
-#			Finley
-#			Oliver
-#		Dogs
-#			Kirby
-#	Outdoor
-#		Dogs
-#			Kirby
-#			Hoover
-#		Goats
-#			Primrose
-#			Joey
+#    Pets
+#    Indoor
+#        Cats
+#            Sophia
+#            Hillary
+#        Rats
+#            Finley
+#            Oliver
+#        Dogs
+#            Kirby
+#    Outdoor
+#        Dogs
+#            Kirby
+#            Hoover
+#        Goats
+#            Primrose
+#            Joey
 #
-#	a grep for Sophia returns:
+#    a grep for Sophia returns:
 #
-#	Indoor
-#		Cats
-#			Sophia
+#    Indoor
+#        Cats
+#            Sophia
 #
-#	a grep for Dogs returns:
+#    a grep for Dogs returns:
 #
-#	Indoor
-#		Dogs
-#			Kirby
-#			Hoover
-#	Outdoor
-#		Dogs
-#			Kirby
-#			Hoover
+#    Indoor
+#        Dogs
+#            Kirby
+#            Hoover
+#    Outdoor
+#        Dogs
+#            Kirby
+#            Hoover
 #
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -68,8 +68,7 @@
 # include whatever mdules we need
 
 import sys
-from string import *
-from re import *
+import re
 
 ###########################################################################
 # global variables
@@ -88,9 +87,9 @@ inputfiles = []
 
 
 def dprint(*vals):
-  global debug
-  if debug != 0:
-    print vals
+    global debug
+    if debug != 0:
+        print vals
 
 
 # usage
@@ -98,16 +97,16 @@ def dprint(*vals):
 # input: none
 # output: simple command usage is printed on the console
 def showUsage():
-  print
-  print "Usage:"
-  print "otlgrep.py [options] pattern [file...] "
-  print "Options"
-  print "    -i            Ignore case"
-  print "    --help        Show help."
-  print "[file...] is zero or more files to search. Wildcards are supported."
-  print "          if no file is specified, input is expected on stdin."
-  print "output is on STDOUT"
-  print
+    print """
+    Usage:
+    otlgrep.py [options] pattern [file...]
+    Options
+        -i            Ignore case
+        --help        Show help.
+    [file...] is zero or more files to search. Wildcards are supported.
+            if no file is specified, input is expected on stdin.
+    output is on STDOUT
+    """
 
 
 # getArgs
@@ -116,27 +115,31 @@ def showUsage():
 # output: possible console output for help, switch variables may be set
 
 def getArgs():
-  global debug, pattern, inputfiles, ignorecase
-  if (len(sys.argv) == 1):
-    showUsage()
-    sys.exit()()
-  else:
-    for i in range(len(sys.argv)):
-      if (i != 0):
-        if   (sys.argv[i] == "-d"): debug = 1	# test for debug flag
-        elif (sys.argv[i] == "-i"): ignorecase = 1	# test for debug flag
-        elif (sys.argv[i] == "-?"):		# test for help flag
-          showUsage()                           # show the help
-          sys.exit()                            # exit
-        elif (sys.argv[i] == "--help"):
-          showUsage()
-          sys.exit()
-        elif (sys.argv[i][0] == "-"):
-          print "Error!  Unknown option.  Aborting"
-          sys.exit()
-        else:                                   # get the input file name
-          if (pattern == ""): pattern = sys.argv[i]
-          else: inputfiles.append(sys.argv[i])
+    global debug, pattern, inputfiles, ignorecase
+    if (len(sys.argv) == 1):
+        showUsage()
+        sys.exit()()
+    else:
+        for i in range(len(sys.argv)):
+            if (i != 0):
+                if (sys.argv[i] == "-d"):
+                    debug = 1  # test for debug flag
+                elif (sys.argv[i] == "-i"):
+                    ignorecase = 1    # test for debug flag
+                elif (sys.argv[i] == "-?"):        # test for help flag
+                    showUsage()                           # show the help
+                    sys.exit()                            # exit
+                elif (sys.argv[i] == "--help"):
+                    showUsage()
+                    sys.exit()
+                elif (sys.argv[i][0] == "-"):
+                    print "Error!  Unknown option.  Aborting"
+                    sys.exit()
+                else:       # get the input file name
+                    if (pattern == ""):
+                        pattern = sys.argv[i]
+                    else:
+                        inputfiles.append(sys.argv[i])
 
 
 # getLineLevel
@@ -144,10 +147,10 @@ def getArgs():
 # input: linein - a single line that may or may not have tabs at the beginning
 # output: returns a number 1 is the lowest
 def getLineLevel(linein):
-    strstart = lstrip(linein)			# find the start of text in line
-    x = find(linein, strstart)			# find the text index in the line
-    n = count(linein, "\t", 0, x)		# count the tabs
-    return(n)					# return the count + 1 (for level)
+    strstart = linein.lstrip()           # find the start of text in line
+    x = linein.find(strstart)            # find the text index in the line
+    n = linein.count("\t", 0, x)         # count the tabs
+    return(n)                    # return the count + 1 (for level)
 
 
 # processFile
@@ -155,38 +158,39 @@ def getLineLevel(linein):
 # input: file - the filehandle of the file we are splitting
 # output: output files
 def processFile(file):
+    global debug, pattern, ignorecase
 
-  global debug, pattern, ignorecase
+    parents = []
+    parentprinted = []
+    for i in range(10):
+        parents.append("")
+        parentprinted.append(0)
 
-  parents = []
-  parentprinted = []
-  for i in range(10):
-    parents.append("")
-    parentprinted.append(0)
-
-  matchlevel = 0
-  line = file.readline()      # read the outline title
-                              # and discard it
-  line = file.readline()      # read the first parent heading
-  while (line !=""):
-    level = getLineLevel(line)
-    parents[level] = line
-    parentprinted[level] = 0
-    if (ignorecase == 1): linesearch = search(pattern,lstrip(rstrip(line)),I)
-    else: linesearch = search(pattern,lstrip(rstrip(line)))
-    if (linesearch != None):
-      matchlevel = level
-      for i in range(level):  # print my ancestors
-        if (parentprinted[i] == 0):
-          print parents[i][:-1]
-          parentprinted[i] = 1
-      print parents[level][:-1] # print myself
-      line = file.readline()
-      while (line != "") and (getLineLevel(line) > matchlevel):
-        print line[:-1]
-        line = file.readline()
-    else:
-      line = file.readline()
+    matchlevel = 0
+    line = file.readline()      # read the outline title
+                                # and discard it
+    line = file.readline()      # read the first parent heading
+    while (line != ""):
+        level = getLineLevel(line)
+        parents[level] = line
+        parentprinted[level] = 0
+        if (ignorecase == 1):
+            linesearch = re.search(pattern, line.strip(), re.I)
+        else:
+            linesearch = re.search(pattern, line.strip())
+        if (linesearch is not None):
+            matchlevel = level
+            for i in range(level):  # print my ancestors
+                if (parentprinted[i] == 0):
+                    print parents[i][:-1]
+                    parentprinted[i] = 1
+            print parents[level][:-1]  # print myself
+            line = file.readline()
+            while (line != "") and (getLineLevel(line) > matchlevel):
+                print line[:-1]
+                line = file.readline()
+        else:
+            line = file.readline()
 
 
 # main
@@ -195,14 +199,14 @@ def processFile(file):
 # output: output files
 
 def main():
-  global inputfiles, debug
-  getArgs()
-  if (len(inputfiles) == 0):
-      processFile(sys.stdin)
-  else:
-    for i in range(len(inputfiles)):
-      file = open(inputfiles[i],"r")
-      processFile(file)
-      file.close()
+    global inputfiles, debug
+    getArgs()
+    if (len(inputfiles) == 0):
+        processFile(sys.stdin)
+    else:
+        for i in range(len(inputfiles)):
+            file = open(inputfiles[i], "r")
+            processFile(file)
+        file.close()
 
 main()
