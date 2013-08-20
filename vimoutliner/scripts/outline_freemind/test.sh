81f3563b4b4faab93f5cc3283fb30b7e794878a6
jostber <jbernts@broadpark.no>
2013-08-20 13:46:44 +0200

    delete

--- vimoutliner/scripts/outline_freemind/test.sh
+++
@@ -1,6 +0,0 @@
-outputdir=/tmp
-#fname=$1
-fname=test.otl
-python outline_freemind.py $fname > $outputdir/test.mm
-python freemind_outline.py $outputdir/test.mm > $outputdir/return.otl
-diff $fname $outputdir/return.otl
