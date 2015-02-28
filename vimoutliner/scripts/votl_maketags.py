fb7c63621abee7aae66e3da1cb8f8ed0a582e149
Matěj Cepl <mcepl@cepl.eu>
2015-02-28 18:33:55 +0100

    Eliminate global variables and don’t nuke files if you don’t have to.

--- vimoutliner/scripts/votl_maketags.py
+++ vimoutliner/scripts/votl_maketags.py
@@ -25,7 +25,6 @@ import re
 
 
 TAGFILENAME = os.path.expanduser("~/.vim/vimoutliner/vo_tags.tag")
-TAGFILENAME_fh = None
 
 TAG_RE = re.compile(r'''
 ^\s*(?P<tagname>_tag_\S+).*
@@ -33,16 +32,11 @@ TAG_RE = re.compile(r'''
     ^\s*(?P<filename>.*)
 ''', re.VERBOSE | re.MULTILINE)
 
-# HashRef containing a map from a filename to its tag names
-# { filename => { tagname => filename } }
-files_to_tags = {}
-# list of all the files left to process
-process_queue = []
 
-def append_tags_to_tagfile(tags):
-    global TAGFILENAME_fh
+def append_tags_to_tagfile(tags, outfile):
     for tag in tags:
-        print("{0}\t{1}\t:1".format(tag, tags[tag]), file=TAGFILENAME_fh)
+        print("{0}\t{1}\t:1".format(tag, tags[tag]), file=outfile)
+
 
 def process_file(filename):
     f = os.path.abspath(filename)
@@ -55,51 +49,54 @@ def process_file(filename):
 
     return f_tags
 
-def create_and_process(filename):
-    global files_to_tags, process_queue
+
+def create_and_process(filename, outfile, queue, filestag):
     filename = os.path.abspath(filename)
 
-    if filename in files_to_tags:
+    if filename in filestag:
         return
 
     basedir = os.path.dirname(filename)
 
     if not os.path.exists(filename):
         os.makedirs(basedir)
-        open(filename, 'w')
-        files_to_tags[filename] = {}
+        open(filename, 'a')
+        filestag[filename] = {}
     else:
         results = process_file(filename)
         for tag in results:
             results[tag] = os.path.abspath(os.path.join(basedir, results[tag]))
-        process_queue.extend(results.values())
+        queue.extend(results.values())
 
-        append_tags_to_tagfile(results)
+        append_tags_to_tagfile(results, outfile)
 
         # let's store all the tags (useful for debugging)
-        files_to_tags[filename] = results
+        filestag[filename] = results
 
 
 def sort_and_dedupe_tagfile():
-    global TAGFILENAME
     sorted_set = sorted(set(open(TAGFILENAME, 'r').readlines()))
     with open(TAGFILENAME, 'w') as tagfile:
         tagfile.writelines(sorted_set)
 
+
 def main():
-    global TAGFILENAME_fh, TAGFILENAME, process_queue
+    # dict containing a map from a filename to its tag names
+    # { filename => { tagname => filename } }
+    files_to_tags = {}
     parser = argparse.ArgumentParser()
     parser.add_argument('files', nargs="+",
                         help='directories with data')
     args = parser.parse_args()
     process_queue = args.files
 
-    TAGFILENAME_fh = open(TAGFILENAME, 'a')
+    tagfile = open(TAGFILENAME, 'a')
 
     for otl_file in process_queue:
-        create_and_process(otl_file)
+        create_and_process(otl_file, tagfile, process_queue,
+                           files_to_tags)
 
-    TAGFILENAME_fh.close()
+    tagfile.close()
     sort_and_dedupe_tagfile()
 
 
